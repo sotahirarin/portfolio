3df1ee50f9084f0b4b808959756bd7fd733451a2
sotahirarin <sota.hirarin@gamil.com>
2024-02-06 11:07:25 +0900

    ハンバーガーボタンの動きを改善

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 // ハンバーガーボタンとドロワー
 $("#js-button-drawer").on("click", function () {
-    $(this).toggleClass("is-checked");
-    $("#js-drawer").slideToggle();
-    $("body").toggleClass("is-fixed");
+  $(this).toggleClass("is-checked");
+  $("#js-drawer").slideToggle();
+  $("body").toggleClass("is-fixed");
 });
 
 // ドロワーのリンクをクリックしたらドロワーを閉じる
 $("#js-drawer a").on("click", function () {
   $("#js-drawer").slideUp();
-  $("body").removeClass("is-fixed");
+  $("body").removeClass("is-checked");
+  $("#js-button-drawer").removeClass("is-checked");
 });
