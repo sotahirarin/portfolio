bf1e0fd09a874f533a15a45f6e968d433d22fded
sotahirarin <156908688+sotahirarin@users.noreply.github.com>
2024-02-06 19:07:54 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // ハンバーガーボタンとドロワー
 $("#js-button-drawer").on("click", function () {
-  $(this).toggleClass("is-checked");
-  $("#js-drawer").slideToggle();
-  $("body").toggleClass("is-fixed");
-});
+    $(this).toggleClass("is-checked");
+    $("#js-drawer").slideToggle();
+    $("body").toggleClass("is-fixed");
+  });
+
+
+  
