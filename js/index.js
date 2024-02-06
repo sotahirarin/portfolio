4c50ec1916555605d297c6279d402f07c702a1ac
sotahirarin <sota.hirarin@gamil.com>
2024-02-06 11:14:45 +0900

    ハンバーガーボタン戻し

--- js/index.js
+++ js/index.js
@@ -4,10 +4,3 @@ $("#js-button-drawer").on("click", function () {
   $("#js-drawer").slideToggle();
   $("body").toggleClass("is-fixed");
 });
-
-// ドロワーのリンクをクリックしたらドロワーを閉じる
-$("#js-drawer a").on("click", function () {
-  $("#js-drawer").slideUp();
-  $("body").removeClass("is-checked");
-  $("#js-button-drawer").removeClass("is-checked");
-});
