74c12dc17f56ee090e9057f1800c46a6a26ee4dd
sotahirarin <sota.hirarin@gamil.com>
2024-02-06 10:42:51 +0900

    ドロワーの動きを修正

--- js/index.js
+++ js/index.js
@@ -3,4 +3,10 @@ $("#js-button-drawer").on("click", function () {
     $(this).toggleClass("is-checked");
     $("#js-drawer").slideToggle();
     $("body").toggleClass("is-fixed");
-  });
+});
+
+// ドロワーのリンクをクリックしたらドロワーを閉じる
+$("#js-drawer a").on("click", function () {
+  $("#js-drawer").slideUp();
+  $("body").removeClass("is-fixed");
+});
