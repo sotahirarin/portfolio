ed21d8f36d81ea7b2e4d66d68d76a60c9a332dcb
sotahirarin <sota.hirarin@gamil.com>
2024-02-20 21:57:21 +0900

    スムーズなスクロールとレスポンシブ対応のドロワーメニューを追加

--- js/index.js
+++ js/index.js
@@ -9,13 +9,15 @@ $("#js-button-drawer").on("click", function () {
 document
   .querySelectorAll('#js-drawer-content a[href^="#"]')
   .forEach(function (link) {
-    link.addEventListener('click', function (e) {
-      document
-        .querySelector("#js-button-drawer")
-        .classList.remove("is-checked");
-      $("#js-drawer").slideToggle();
-      document
-        .querySelector("body")
-        .classList.remove("is-fixed");
-    });
+    if ($(window).width() < 768) {
+      link.addEventListener('click', function (e) {
+        document
+          .querySelector("#js-button-drawer")
+          .classList.remove("is-checked");
+        $("#js-drawer").slideToggle();
+        document
+          .querySelector("body")
+          .classList.remove("is-fixed");
+      });
+    }
   });
