bbd0e3019054336452b7df1f824dcbec1dea819b
sotahirarin <sota.hirarin@gamil.com>
2024-02-20 21:07:52 +0900

    画像とスタイルの削除

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 // ハンバーガーボタンとドロワー
 $("#js-button-drawer").on("click", function () {
-    $(this).toggleClass("is-checked");
-    $("#js-drawer").slideToggle();
-    $("body").toggleClass("is-fixed");
+  $(this).toggleClass("is-checked");
+  $("#js-drawer").slideToggle();
+  $("body").toggleClass("is-fixed");
+});
+
+
+document
+  .querySelectorAll('#js-drawer-content a[href^="#"]')
+  .forEach(function (link) {
+    link.addEventListener('click', function (e) {
+      document
+        .querySelector("#js-button-drawer")
+        .classList.remove("is-checked");
+      $("#js-drawer").slideToggle();
+      document
+        .querySelector("body")
+        .classList.remove("is-fixed");
+    });
   });
-
-
-  
